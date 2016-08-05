08f1636bd93f54a736e1a587f529928410bcebd5
myles <myles@Myles-OConnor-iMac-27.local>
2016-08-05 15:59:07 -0700

    Initial commit

---
+++ app/components/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    
+  }
+
+  render() {
+    return (
+      <div id="content">
+        <h1>&nbsp;</h1>
+        <h2>Welcome!</h2>
+        <ul>
+          <li><a href="http://brunch.io">Brunch homepage</a></li>
+          <li><a href="https://facebook.github.io/react/">React.js homepage</a></li>
+        </ul>
+      </div>
+    );
+  }
+}
